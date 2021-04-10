fd6958f9637962931ac75b417645cf519328c9ab
unknown <evasquezcam@gmail.com>
2021-04-10 00:20:46 -0400

    iniciando clases en carpetas

---
+++ LogicLayer/resultado pruebas de laboratorio/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLayer.resultado_pruebas_de_laboratorio
+{
+    class Class1
+    {
+    }
+}
