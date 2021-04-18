415510f0d6c4f0d1536402d8b85cb67523f20cb3
JDerekZ <jplasencia03@gmail.com>
2021-04-18 16:21:27 -0400

    Añadiendo Frms de Paciente

---
+++ Sistema gestor de pacientes/forms/pacientes/FrmEditarPaciente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_gestor_de_pacientes.forms.pacientes
+{
+    public partial class FrmEditarPaciente : Form
+    {
+        public FrmEditarPaciente()
+        {
+            InitializeComponent();
+        }
+    }
+}
