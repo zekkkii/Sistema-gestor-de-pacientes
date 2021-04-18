2f13517b966d058c7eb390311b9c7b4321eb1ed8
unknown <evasquezcam@gmail.com>
2021-04-18 16:21:51 -0400

    medicos logic layer terminado

---
+++ LogicLayer/medicos/Registro.cs
@@ -0,0 +1,35 @@
+﻿using DataBase.DB.medicos;
+using DataBase.DB.models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLayer.medicos
+{
+    public class Registro
+    {
+        public Service_Registrar iniciarServicio { get; set; }
+
+        public Registro(SqlConnection connection)
+        {
+            iniciarServicio = new Service_Registrar(connection);
+        }
+
+
+        public bool registro(Medico user)
+        {
+            bool registrar = iniciarServicio.registrar(user);
+
+            if (registrar)
+            {
+                // se creo el usuario
+                return true;
+            }
+            // no se creo el usuario
+            return false;
+        }
+    }
+}
---
+++ LogicLayer/medicos/VerListado.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DataBase.DB.medicos;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace LogicLayer.medicos
+{
+    public class VerListado
+    {
+        public Service_Listar iniciarServicio { get; set; }
+
+        public VerListado(SqlConnection connection)
+        {
+            iniciarServicio = new Service_Listar(connection);
+        }
+
+        public DataTable listar()
+        {
+            // se llama a la DB y se cargan los datos obtenidos en un datable que luego se carga al datagrid            
+            DataTable data = iniciarServicio.listar();
+            return data;
+        }
+    }
+}
