106ce0cf0637260d1e6648bcaf1ededbcc058336
unknown <evasquezcam@gmail.com>
2021-04-18 16:50:58 -0400

    resultados pruebas de lab logic layer terminado

---
+++ LogicLayer/resultado pruebas de laboratorio/Buscar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+ using DataBase.DB.resultados_pruebas_de_laboratorio;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace LogicLayer.resultado_pruebas_de_laboratorio
+{
+    public class Buscar
+    {
+        public Service_Buscar iniciarServicio { get; set; }
+
+        public Buscar(SqlConnection connection)
+        {
+            iniciarServicio = new Service_Buscar(connection);
+        }
+
+        public DataTable listar(string cedula)
+        {
+            // se llama a la DB y se cargan los datos obtenidos en un datable que luego se carga al datagrid            
+            DataTable data = iniciarServicio.listar(cedula);
+            return data;
+        }
+    }
+}
---
+++ LogicLayer/resultado pruebas de laboratorio/Llenar_Resultados.cs
@@ -0,0 +1,29 @@
+﻿using DataBase.DB.resultados_pruebas_de_laboratorio;
+using System;
+using DataBase.DB.models;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLayer.resultado_pruebas_de_laboratorio
+{
+    public class Llenar_Resultados
+    {
+        public Service_Llenar_resultados iniciarServicio { get; set; }
+
+        public Llenar_Resultados(SqlConnection connection)
+        {
+            iniciarServicio = new Service_Llenar_resultados(connection);
+        }
+
+        public bool editar(int id, string dataResultados)
+        {
+            bool fueExitoso = iniciarServicio.llenar(id, dataResultados);
+            if (fueExitoso) return true;
+            //si no se borro devuelve false
+            return false;
+        }
+    }
+}
